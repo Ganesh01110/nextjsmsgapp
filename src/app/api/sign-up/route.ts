cd34456b898b12c121a79d4c4cef8693f8cea59e
Ganesh sahu <sahug8279@gmail.com>
2024-06-16 13:18:10 +0530

    user chack by username annd email

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -8,6 +8,50 @@ export async function POST(request:Request){
 
     try{
       const {username,email,password} = await request.json()
+
+    //   case-1 if username exist finding by username
+      const existingUserVeryfiedByUserName = await UserModel.findOne({
+        username,
+        isVerified:true
+      })
+
+      if(existingUserVeryfiedByUserName ){
+        return Response.json({
+            success:false,
+            message:"username is already taken",
+        },{status:400})
+      }
+
+    //   case-2 finding username by email
+      const existingUserByemail = await UserModel.findOne({ 
+        email
+      })
+
+      const verifyCode = Math.floor(100000 + Math.random() * 900000).toString
+
+      if(existingUserByemail) {
+          true // TODO:
+      }else{
+          const hashedPassword = await bcrypt.hash(password,10)
+          const expiryDate = new Date()
+          expiryDate.setHours(expiryDate.getHours() + 1)
+
+         const newUser =  new UserModel({
+            username,
+            email,
+            password: hashedPassword ,
+            verifyCode,
+            verifyCodeExpire:expiryDate,
+            isVerified:false,
+            isAcceptingMessage:true,
+            messages: []
+          })
+
+          await newUser.save()
+      }
+
+      // send verification email
+
     }catch(error){
        console.error('error registering user',error)
        return Response.json(
