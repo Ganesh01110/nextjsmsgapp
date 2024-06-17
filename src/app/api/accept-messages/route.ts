7ebfccf73525c52255f50a2fa30a004a6deeda19
Ganesh sahu <sahug8279@gmail.com>
2024-06-17 19:12:45 +0530

    added get method to check status of accept-message

--- src/app/api/accept-messages/route.ts
+++ src/app/api/accept-messages/route.ts
@@ -57,3 +57,48 @@ export async function POST(request: Request) {
       );
     }
   }
+
+  export async function GET(request: Request) {
+    // Connect to the database
+    await dbConnect();
+  
+    // Get the user session
+    const session = await getServerSession(authOptions);
+    const user : User = session?.user as User;
+  
+    // Check if the user is authenticated
+    if (!session || !user) {
+      return Response.json(
+        { success: false, message: 'Not authenticated' },
+        { status: 401 }
+      );
+    }
+  
+    try {
+      // Retrieve the user from the database using the ID
+      const foundUser = await UserModel.findById(user._id);
+  
+      if (!foundUser) {
+        // User not found
+        return Response.json(
+          { success: false, message: 'User not found' },
+          { status: 404 }
+        );
+      }
+  
+      // Return the user's message acceptance status
+      return Response.json(
+        {
+          success: true,
+          isAcceptingMessages: foundUser.isAcceptingMessage,
+        },
+        { status: 200 }
+      );
+    } catch (error) {
+      console.error('Error retrieving message acceptance status:', error);
+      return Response.json(
+        { success: false, message: 'Error retrieving message acceptance status' },
+        { status: 500 }
+      );
+    }
+  }
