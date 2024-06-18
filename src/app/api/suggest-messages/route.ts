3e41c7fe2caad3d99bea45357fcc0b055b9e1ff0
Ganesh sahu <sahug8279@gmail.com>
2024-06-18 23:34:49 +0530

    add post method try catch block and catching error with if else

--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -9,3 +9,24 @@ const openai = new OpenAI({
 // setting run time as edge as next goes timebout
 export const runtime = 'edge';
 
+export async function POST(req: Request) { 
+    try {
+
+        
+
+    }catch(error){
+        if (error instanceof OpenAI.APIError) {
+            // OpenAI API error handling
+            const { name, status, headers, message } = error;
+            return NextResponse.json({ name, status, headers, message }, { status });
+          } 
+          
+        else {
+            // General error handling
+            console.error('An unexpected error occurred:', error);
+            throw error;
+          }
+
+    }
+}
+
