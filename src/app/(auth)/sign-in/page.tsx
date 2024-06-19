840f7e270771fd0e85e6a93fb1ddd7b981bde0ab
Ganesh sahu <sahug8279@gmail.com>
2024-06-19 22:34:16 +0530

    use state and zod validation to signup

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,6 +1,38 @@
-import React from 'react'
+'use client';
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z  from "zod"
+import Link from "next/link"
+import { useEffect, useState } from 'react';
+import { useDebounceValue } from 'usehooks-ts'
+import { useToast } from '@/components/ui/use-toast';
+import { useRouter } from 'next/navigation';
+import { signUpSchema } from '@/schemas/signUpSchemas';
+
+
 
 const page = () => {
+  const [username, setUsername] = useState('');
+  const [usernameMessage, setUsernameMessage] = useState('');
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const debouncedUsername = useDebounceValue(username, 300);
+
+  const router = useRouter();
+  const { toast } = useToast();
+
+// zod validator
+  const form = useForm<z.infer<typeof signUpSchema>>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      username: '',
+      email: '',
+      password: '',
+    },
+  });
+
   return (
     <div>page</div>
   )
