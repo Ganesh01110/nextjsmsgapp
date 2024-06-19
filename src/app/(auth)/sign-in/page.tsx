d583739a0cd04b01a76b923dd55019a75cd3e33c
Ganesh sahu <sahug8279@gmail.com>
2024-06-19 23:02:03 +0530

    add debounce handling on name and axios get with error handling

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -8,7 +8,9 @@ import { useEffect, useState } from 'react';
 import { useDebounceValue } from 'usehooks-ts'
 import { useToast } from '@/components/ui/use-toast';
 import { useRouter } from 'next/navigation';
-import { signUpSchema } from '@/schemas/signUpSchemas';
+import { signupSchema } from '@/schemas/signUpSchemas';
+import axios,{AxiosError} from 'axios'
+import { ApiResponse } from '@/types/ApiResponse';
 
 
 
@@ -24,8 +26,8 @@ const page = () => {
   const { toast } = useToast();
 
 // zod validator
-  const form = useForm<z.infer<typeof signUpSchema>>({
-    resolver: zodResolver(signUpSchema),
+  const form = useForm<z.infer<typeof signupSchema>>({
+    resolver: zodResolver(signupSchema),
     defaultValues: {
       username: '',
       email: '',
@@ -33,7 +35,36 @@ const page = () => {
     },
   });
 
+  useEffect(() =>{
+    const checkUsernameUnique = async () => {
+      if (debouncedUsername) {
+        setIsCheckingUsername(true)
+        setUsernameMessage('')
+        try {
+          const response = await axios.get<ApiResponse>(
+            `/api/check-username-unique?username=${debouncedUsername}`
+          );
+          setUsernameMessage(response.data.message);
+        } catch (error) {
+          const axiosError = error as AxiosError<ApiResponse>;
+          setUsernameMessage(
+            axiosError.response?.data.message ?? 'Error checking username'
+          );
+          
+        }finally {
+          setIsCheckingUsername(false);
+        }
+        
+      } 
+      
+    }
+
+    checkUsernameUnique();
+  },[debouncedUsername])
+
   return (
+
+
     <div>page</div>
   )
 }
--- src/schemas/signUpSchemas.ts
+++ src/schemas/signUpSchemas.ts
@@ -8,7 +8,7 @@ export const usernameValidation = z
 
 
 export const signupSchema = z.object({
-    usesrname:usernameValidation ,
+    username:usernameValidation ,
     email:z.string().email({message:'invalid email address'}),
     password: z.string().min(6, {message:"password must be at least 6 characters"}),
 })
